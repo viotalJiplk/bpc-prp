0ce531c1d84c0a0de4f40b19d5c3f19d6a4e3d58
50jt4 <50jt4.50jt4@gmail.com>
2025-03-19 21:38:09 +0100

    feat(io_node): button getter, LED setter, comments

--- include/io_node.hpp
+++ include/io_node.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/u_int8.hpp>
 #include "motors.hpp"
@@ -11,21 +12,33 @@ namespace nodes {
     public:
         // Constructor
         IoNode(std::shared_ptr<Motors> motor, std::shared_ptr<LineNode> line);
+
         // Destructor (default)
         ~IoNode() override = default;
 
         // Function to retireve the last pressed button value
         int get_button_pressed() const;
 
+        // Functions to set LED colors
+        void set_led_color(uint8_t led_number, uint8_t R, uint8_t G, uint8_t B);
+        void set_all_leds_color(uint8_t R, uint8_t G, uint8_t B);
+
     private:
+        // For experiments launched from button callback function
         std::shared_ptr<LineNode> line_;
         std::shared_ptr<Motors> motors_;
         std::shared_ptr<rclcpp::Subscription<std_msgs::msg::UInt8_<std::allocator<void>>>> subscriber;
+
         // Variable to store the last received button press value
         int button_pressed_ = -1;
 
+        // Variable to store current LEDs state
+        uint8_t leds[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
         // Subscriber for button press messages
         rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr button_subscriber_;
+
+        // Publisher for LED RGB values
         rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr led_publisher_;
 
         // Callback - preprocess received message
--- src/nodes/io_node.cpp
+++ src/nodes/io_node.cpp
@@ -3,32 +3,64 @@
 
 namespace nodes {
     IoNode::IoNode(std::shared_ptr<Motors> motor, std::shared_ptr<LineNode> line): Node("buttonsReader") {
-            // Initialize the subscriber
-            line_ = line;
-            motors_ = motor;
-             button_subscriber_ = this->create_subscription<std_msgs::msg::UInt8>(
-                Topic::buttons, 1, std::bind(&IoNode::on_button_callback, this, std::placeholders::_1));
-            led_publisher_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>(Topic::set_rgb_leds, 1);
+        // Initialize the subscriber for buttons
+        button_subscriber_ = this->create_subscription<std_msgs::msg::UInt8>(
+            Topic::buttons, 1, std::bind(&IoNode::on_button_callback, this, std::placeholders::_1)
+        );
+
+        // Initialize the publisher for LEDs
+        led_publisher_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>(Topic::set_rgb_leds, 1);
     }
 
-    int IoNode::get_button_pressed() const{
+    int IoNode::get_button_pressed() const {
+        return IoNode::button_pressed_;
     }
+
+    void IoNode::set_led_color(uint8_t led_number, uint8_t R, uint8_t G, uint8_t B) {
+        if(led_number >= 3) return; // we have only 3 LEDs on our robot
+        std_msgs::msg::UInt8MultiArray leds = std_msgs::msg::UInt8MultiArray();
+        switch(led_number) {
+            case 0: leds.data = {R, G, B, IoNode::leds[3], IoNode::leds[4], IoNode::leds[5], IoNode::leds[6], IoNode::leds[7], IoNode::leds[8]}; break;
+            case 1: leds.data = {IoNode::leds[0], IoNode::leds[1], IoNode::leds[2], R, G, B, IoNode::leds[6], IoNode::leds[7], IoNode::leds[8]}; break;
+            case 2: leds.data = {IoNode::leds[0], IoNode::leds[1], IoNode::leds[2], IoNode::leds[3], IoNode::leds[4], IoNode::leds[5], R, G, B}; break;
+        }
+        led_publisher_->publish(leds);
+    }
+
+    void IoNode::set_all_leds_color(uint8_t R, uint8_t G, uint8_t B) {
+        std_msgs::msg::UInt8MultiArray leds = std_msgs::msg::UInt8MultiArray();
+        leds.data = {R, G, B, R, G, B, R, G , B};
+        led_publisher_->publish(leds);
+    }
+
     void IoNode::on_button_callback(std_msgs::msg::UInt8_<std::allocator<void>>::SharedPtr msg) {
+        // processing message to button number - main purpose of this function
+        IoNode::button_pressed_ = msg->data;
+
+
+        // TEMPORARY: LAUNCHING EXPERIMENTS VIA BUTTONS
+        // --------------------------------------------
+
         std::cout << msg->data << std::endl;
-        auto leds = std_msgs::msg::UInt8MultiArray();
+
         if (msg->data == 0) {
-            leds.data = {255, 0, 0};
+            // 1st button + red light
+            set_all_leds_color(255, 0, 0);
             line_->calibrationStart();
             motors_->setMotorsSpeed(120, 136);
             // motors_->setMotorsSpeed(140, 0);
         } else if (msg->data == 1) {
-            leds.data = {0, 255, 0};
+            // 2nd button + green light
+            set_all_leds_color(0, 255, 0);
             // motors_->setMotorsSpeed(140, 140);
         } else {
-            leds.data = {0, 0, 255};
+            // 3rd button + blue light
+            set_all_leds_color(0, 0, 255);
             // motors_->setMotorsSpeed(0, 140);
         }
-        led_publisher_->publish(leds);
+
+        // ------------------------
+        // END OF TEMPORARY SECTION
+
     }
-    // ...
 }
